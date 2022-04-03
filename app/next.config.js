1d4b24f4ad3578301d699256341b28f462874523
murata0531 <coless115@gmail.com>
2022-04-03 15:39:24 +0900

    appを削除

--- app/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  webpack: (config, { isServer }) => {
-    config.experiments = {
-      asyncWebAssembly: true,
-    };
-    config.output.webassemblyModuleFilename = (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
-    return config;
-  },
-};
